7c787fb27b6db69c7351e762251f70b5a66d0eaf
will <will@stockweezy.corp.dropbox.com>
2011-07-31 19:14:05 -0700

    sexier p2 in python

--- python/p2.py
+++ python/p2.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python
 
+from itertools import ifilter, takewhile
+
 def fib():
     (a, b) = (1, 2)
     while True:
         yield a
         (a, b) = (b, a + b)
 
-def fib_below_4m():
-    for x in fib():
-        if x > 4000000:
-            break
-        yield x
-
-print sum((x for x in fib_below_4m() if x % 2 == 0))
+print sum(ifilter(lambda x : x % 2 == 0, takewhile(lambda x : x < 4000000, fib())))
