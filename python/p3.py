e3a85654a29cb09d01de3c2df747e0e385b4fb5d
Will Stockwell <bigwill@gmail.com>
2012-07-28 20:15:51 -0700

    minor p3 cleanup

--- python/p3.py
+++ python/p3.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 
 def prime_factors(n):
-    fs = []
     p = 2
     while n > 1:
         if n % p == 0:
-            fs.append(p)
+            yield p
             n /= p
         else:
             p += 1
-    return fs
 
 print max(prime_factors(600851475143))
     
