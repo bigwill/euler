017a3174adbddc1efbbbad9df26d23be7151ad42
Will Stockwell <bigwill@gmail.com>
2012-07-28 20:30:12 -0700

    more memory efficient p7

--- python/p7.py
+++ python/p7.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python
 
-def prime_factors(n):
-    fs = []
-    p = 2
-    while n > 1:
-        if n % p == 0:
-            fs.append(p)
-            n /= p
-        else:
-            p += 1
-    return fs
+from math import sqrt
+
+# Basically jacked from: http://www.daniweb.com/software-development/python/code/216880
+def is_prime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if not n & 1:
+        return False
+    for x in xrange(3, int(sqrt(n))+1, 2):
+        if n % x == 0:
+            return False
+    return True
 
 c = 0
 x = 2
 
 while True:
-    if len(prime_factors(x)) == 1:
+    if is_prime(x):
         c += 1
         if c == 10001:
             print x
